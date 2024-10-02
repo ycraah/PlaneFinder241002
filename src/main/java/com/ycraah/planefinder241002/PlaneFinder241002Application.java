30e2d8de3362d077fef6ccf00a5244919ca16fd4
김예찬 <ycraah@gamil.com>
2024-10-02 19:18:09 +0900

    도메인 클래스 정의하기

---
+++ src/main/java/com/ycraah/planefinder241002/PlaneFinder241002Application.java
@@ -0,0 +1,15 @@
+package com.ycraah.planefinder241002;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneFinder241002Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlaneFinder241002Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/ycraah/planefinder241002/PlaneFinder241002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycraah.planefinder241002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinder241002ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
